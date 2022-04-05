ce7a5a0ea9de2b86c1d3852bdaf719a270ba944f
Tomi <74303735+Tomi-1997@users.noreply.github.com>
2022-04-05 22:52:58 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,27 @@
+/** *
+ * AUTHORS: <Tom Inbar>
+ * 
+ * Date: 2022-04
+ */
+
+#include "doctest.h"
+#include "Matrix.hpp"
+using namespace ariel;
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+TEST_CASE("Good input") 
+{
+}
+
+TEST_CASE("Bad input") 
+{
+}
+
+
+TEST_CASE("Edge cases")
+{
+}
